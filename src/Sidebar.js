ab99e58563ec16a5354cf1baf75c3d1f17c37177
DylanParenzee <dylanparenzee@hotmail.com>
2022-12-13 23:19:38 +1100

    added sided bar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="links-container show-container">
+      <ul className="links">
+        {links.map((item) => {
+          const { id, url, text } = item;
+          return (
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
