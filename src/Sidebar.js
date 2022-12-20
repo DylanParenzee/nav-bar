4d6ad241e26fbe346cb2e1641b3d9537c978907f
DylanParenzee <dylanparenzee@hotmail.com>
2022-12-20 17:46:07 +1100

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { links, social } from "./data";
 
 const Sidebar = () => {
+    
   return (
     <div className="links-container show-container">
       <ul className="links">
